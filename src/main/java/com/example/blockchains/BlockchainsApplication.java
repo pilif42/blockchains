54fe381429e1681ecb9dd9de6b4323aad992ebb2
Philippe Brossier <brossierp@gmail.com>
2022-10-30 16:59:01 +0000

    skeleton app

---
+++ src/main/java/com/example/blockchains/BlockchainsApplication.java
@@ -0,0 +1,13 @@
+package com.example.blockchains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blockchains/BlockchainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockchains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
